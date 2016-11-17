4862deb996f369a9c8ce984983dbbfcd3f56ccad
Clara Raubertas <clara@parallactic.com>
2016-11-17 15:20:02 -0600

    twitter oauth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -9,7 +9,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => @kind) if is_navigational_format?
     else
-      set_flash_message(:notice, :failure, :kind => @kind) if is_navigational_format?
+      set_flash_message(:alert, :failure, :kind => @kind, :reason => failure_message) if is_navigational_format?
     end
   end
 
