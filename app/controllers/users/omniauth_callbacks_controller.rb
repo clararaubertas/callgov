08fd86456d2fc5b3aaa3a8438b421b8e471e11b9
Clara Raubertas <clara@parallactic.com>
2016-11-17 15:09:03 -0600

    oauth route

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,7 +3,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   
   
   def callback
-    # You need to implement the method below in your model (e.g. app/models/user.rb)nn
+    # You need to implement the method below in your model (e.g. app/models/user.rb)
     @user = User.from_omniauth(request.env["omniauth.auth"])
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
@@ -13,7 +13,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     end
   end
 
-  def google
+  def google_oauth2
     callback
   end
 
