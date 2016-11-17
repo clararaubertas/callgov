226c947d01de1453e2c43813368a106df9bf7009
Clara Raubertas <clara@parallactic.com>
2016-11-17 14:46:49 -0600

    oauth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,14 +3,13 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   
   
   def callback
-    # You need to implement the method below in your model (e.g. app/models/user.rb)
+    # You need to implement the method below in your model (e.g. app/models/user.rb)nn
     @user = User.from_omniauth(request.env["omniauth.auth"])
-
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, :kind => params [:kind]) if is_navigational_format?
+      set_flash_message(:notice, :success, :kind => params[:kind]) if is_navigational_format?
     else
-      set_flash_message(:notice, :failure, :kind => params [:kind]) if is_navigational_format?
+      set_flash_message(:notice, :failure, :kind => params[:kind]) if is_navigational_format?
     end
   end
 
