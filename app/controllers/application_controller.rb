ffad9f865d1664e9ca491084623541e4e7a05602
Clara Raubertas <clara@parallactic.com>
2016-11-17 18:46:47 -0600

    redirect after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,8 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to new_user_registration_path, :alert => exception.message
   end
+  def after_sign_in_path_for(user)
+    new_calling_script_url
+  end
 
 end
