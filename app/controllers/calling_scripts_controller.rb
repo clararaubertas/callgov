c5fe6ab39fcdb8d04c8efc6aa9a8dcbb3028f240
Clara Raubertas <clara@parallactic.com>
2016-11-17 19:35:20 -0600

    ID via bioguide

--- app/controllers/calling_scripts_controller.rb
+++ app/controllers/calling_scripts_controller.rb
@@ -12,7 +12,7 @@ class CallingScriptsController < ApplicationController
   # GET /calling_scripts/1.json
   def show
     if params[:rep_id]
-      @representative = Sunlight::Legislator.all_where(:fec_id => params[:rep_id]).first
+      @representative = Sunlight::Legislator.all_where(:bioguide_id => params[:rep_id]).first
       @substitutions = {:representative => "#{@representative.first_name}, #{@representative.last_name}", :constituent => params[:name], :city => params[:address]}
     elsif params[:address]
       @representatives = Sunlight::Legislator.all_for(:address => params[:address])
