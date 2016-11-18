2c0432d869d82b8b2d640718139f8e297ebce35d
Clara Raubertas <clara@parallactic.com>
2016-11-17 19:22:25 -0600

    route fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   
-  resources :calling_scripts
+  resources :calling_scripts do
+    member do 
+      post 'show'
+      get 'show'
+    end
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
