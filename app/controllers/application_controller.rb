a783df062cb66a4aadda96b675ac4bbb348aa4c4
Clara Raubertas <clara@parallactic.com>
2016-11-17 18:49:20 -0600

    redirect after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to new_user_registration_path, :alert => exception.message
   end
+
   def after_sign_in_path_for(user)
     new_calling_script_url
   end
