7f9dc639282e8275bb52f4183bf450a9f7a63ea4
Clara Raubertas <clara@parallactic.com>
2016-11-19 16:50:45 -0600

    user credit

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,24 +12,27 @@ class User < ActiveRecord::Base
     user.email = auth.info.email
     user.password = Devise.friendly_token[0,20]
     user.picture = auth.info.image
-    if auth.provider == 'facebook'
-      user.profile = "http://facebook.com/#{auth.uid}"
-      user.picture = "http://res.cloudinary.com/dm0czpc8q/image/facebook/c_thumb,e_improve,g_face,h_90,r_max,w_90/#{auth.uid}.png"
-    elsif auth.provider == 'twitter'
-      user.profile = auth.info.try(:urls).try(:Twitter)
-      user.picture = "http://res.cloudinary.com/dm0czpc8q/image/twitter/c_thumb,e_improve,g_face,h_90,r_max,w_90/#{auth.uid}.png"
-    elsif auth.provider == 'google_oauth2'
+    user.set_image_and_profile(auth.provider, auth)
+  end
+  end
+
+  def set_image_and_profile(provider, auth)
+    if provider == 'facebook'
+      self.profile = "http://facebook.com/#{auth.uid}"
+      self.picture = "http://res.cloudinary.com/dm0czpc8q/image/facebook/c_thumb,e_improve,g_face,h_90,r_max,w_90/#{auth.uid}.png"
+    elsif provider == 'twitter'
+      self.profile = auth.info.try(:urls).try(:Twitter)
+      self.picture = "http://res.cloudinary.com/dm0czpc8q/image/twitter/c_thumb,e_improve,g_face,h_90,r_max,w_90/#{auth.uid}.png"
+    elsif provider == 'google_oauth2'
       url = URI.parse("http://plus.google.com/#{auth.uid}") 
       if Net::HTTP.new(url.host, url.port).request_head(url.path).code == "200"
-          user.profile = url
-          user.picture = "http://res.cloudinary.com/dm0czpc8q/image/gplus/c_thumb,e_improve,g_face,h_90,r_max,w_90/#{auth.uid}.png"
+        self.profile = url
+        self.picture = "http://res.cloudinary.com/dm0czpc8q/image/gplus/c_thumb,e_improve,g_face,h_90,r_max,w_90/#{auth.uid}.png"
       else
-        user.picture = "http://res.cloudinary.com/dm0czpc8q/image/fetch/c_thumb,e_improve,g_face,h_90,r_max,w_90/#{user.picture}"
+        self.picture = "http://res.cloudinary.com/dm0czpc8q/image/fetch/c_thumb,e_improve,g_face,h_90,r_max,w_90/#{self.picture}.png"
       end
     end
-
   end
-end
   
 end
  
