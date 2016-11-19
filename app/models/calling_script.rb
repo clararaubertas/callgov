047483a4ff84d86e716a0d41f428aae5f2328bcf
Clara Raubertas <clara@parallactic.com>
2016-11-19 15:40:23 -0600

    visual tweaks and view cleanup

--- app/models/calling_script.rb
+++ app/models/calling_script.rb
@@ -2,4 +2,9 @@ class CallingScript < ActiveRecord::Base
   validates_presence_of :content, :topic, :user, :summary
   belongs_to :user
   has_many :calls
+
+  def called_yet?(calling_user, ip)
+    Call.find_by_calling_script_id_and_user_id(id, (calling_user.try(:id) || ip)).present?
+  end
+  
 end
